9d52634cc4af4eb192b1a2d9c617d0ba2d1f6318
QH27 <2151060231@e.tlu.edu.vn>
2024-05-10 22:39:23 +0700

    Create database class and apply singleton pattern to roomdb

---
+++ app/src/main/java/com/example/contactmanager/Database/ContactDatabase.java
@@ -0,0 +1,30 @@
+package com.example.contactmanager.Database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import com.example.contactmanager.DAO.ContactDAO;
+import com.example.contactmanager.Entity.Contacts;
+
+@Database(entities = {Contacts.class}, version = 1)
+public abstract class ContactDatabase extends RoomDatabase {
+    public abstract ContactDAO getContactDAO();
+
+    // Singleton Pattern
+    private static ContactDatabase dbInstance;
+
+    public static synchronized  ContactDatabase getInstance(Context context){
+        if(dbInstance == null){
+            dbInstance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    ContactDatabase.class,
+                    "contacts_db"
+            ).fallbackToDestructiveMigration()
+                    .build();
+        }
+        return dbInstance;
+    }
+}
