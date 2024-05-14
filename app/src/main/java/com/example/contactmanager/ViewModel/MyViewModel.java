abf06baa13ba5e3a586c856339e6291a737ebd6c
QH27 <2151060231@e.tlu.edu.vn>
2024-05-14 09:41:57 +0700

    Create ViewModel

---
+++ app/src/main/java/com/example/contactmanager/ViewModel/MyViewModel.java
@@ -0,0 +1,36 @@
+package com.example.contactmanager.ViewModel;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.example.contactmanager.Entity.Contacts;
+import com.example.contactmanager.Repository.Repository;
+
+import java.util.List;
+
+public class MyViewModel extends AndroidViewModel {
+    private Repository myRepository;
+    private LiveData<List<Contacts>> allContacts;
+
+    public MyViewModel(@NonNull Application application, Repository myRepository) {
+        super(application);
+        this.myRepository = new Repository(application);
+    }
+
+    public LiveData<List<Contacts>> getAllContacts() {
+        allContacts = myRepository.getAllContacts();
+        return allContacts;
+    }
+
+    public void addNewContact(Contacts contacts){
+        myRepository.addContact(contacts);
+    }
+
+    public void deleteContact(Contacts contacts){
+        myRepository.deleteContact(contacts);
+    }
+}
